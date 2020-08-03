7fc1462bf552eb22569fd38d0d7a61b2ce85eb14
Jacob France <jacobfrance28@gmail.com>
2020-08-03 21:11:19 +1200

    didn't save Trick.cpp on last commit/merge.

--- Src/Trick.cpp
+++ Src/Trick.cpp
@@ -12,25 +12,6 @@ Trick::~Trick()
 
 }
 
-bool Trick::executeTrick(int riderTrickSkill)
-{
-	//Difficult tricks might have difficulty of 100
-	//if rider has trick skill of 10 then they only have a 10% chance of landing
-	//there is room for this to become more complex later on but this is just a tribute
-	int i = rand() % this->difficulty;
-	cout << "random number " << i << endl;
-
-	if (i <= riderTrickSkill)
-	{
-		cout << "Trick Landed" << endl;
-	}
-	else
-	{
-		cout << "You Bailed" << endl;
-	}
-	cin >> i;
-	return riderTrickSkill > i;
-}
 
 void Trick::printTrick()
 {
